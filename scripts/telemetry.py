731bae081d65bac23e4d49c8dba68a60c2d4af4d
ADARSH VARMA <152641433+varmaadarsh@users.noreply.github.com>
2026-07-21 11:51:09 +0530

    Implement telemetry script for GitHub contributions
    Add a script to fetch GitHub user contributions using GraphQL API.

--- scripts/telemetry.py
+++ scripts/telemetry.py
@@ -1,1 +1,44 @@
+import os
+import requests
 
+USERNAME = "varmaadarsh"
+
+TOKEN = os.environ.get("GH_TOKEN")
+
+query = """
+query($username:String!){
+  user(login:$username){
+    contributionsCollection{
+      contributionCalendar{
+        totalContributions
+        weeks{
+          contributionDays{
+            contributionCount
+            date
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+variables = {
+    "username": USERNAME
+}
+
+headers = {
+    "Authorization": f"Bearer {TOKEN}"
+}
+
+response = requests.post(
+    "https://api.github.com/graphql",
+    json={
+        "query": query,
+        "variables": variables
+    },
+    headers=headers
+)
+
+print(response.status_code)
+print(response.json())
